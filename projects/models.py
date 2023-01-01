23a5cfb1c45da18b43e1daedf1afb3eb57aef091
mohamedreda <m.elshafaie123@gmail.com>
2023-01-01 21:16:47 +0200

    Member Activity Epic

--- projects/models.py
+++ projects/models.py
@@ -16,18 +16,25 @@ due = (
 )
 
 # Create your models here.
-
-
-
-class Task(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+class Project(models.Model):
+    name = models.CharField(max_length=80)
+    slug = models.SlugField('shortcut', blank=True)
     assign = models.ManyToManyField(User)
-    task_name = models.CharField(max_length=80)
+    efforts = models.DurationField()
     status = models.CharField(max_length=7, choices=status, default=1)
-    due = models.CharField(max_length=7, choices=due, default=1)
+    dead_line = models.DateField()
+    team = models.ForeignKey('register.Team', on_delete=models.CASCADE)
+    complete_per = models.FloatField(max_length=2, validators = [MinValueValidator(0), MaxValueValidator(100)])
+    description = models.TextField(blank=True)
+
+    add_date = models.DateField(auto_now_add=True)
+    upd_date = models.DateField(auto_now_add=False, auto_now=True)
 
     class Meta:
-        ordering = ['project', 'task_name']
+        ordering = ['name']
 
     def __str__(self):
-        return(self.task_name)
+        return (self.name)
+
+
+
